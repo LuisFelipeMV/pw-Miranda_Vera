075ce63854b5498438967d2765636645c1f67877
Felipe Miranda <l_felipe-mv97@hotmail.com>
2022-03-03 08:24:42 -0500

    Crecion de config para mongom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import express from 'express'
-const app = express() 
+import app from "./app";
+import './database';
 
-app.listen(3000)
- console.log( 'Server listen on port ', 3000)
+app.listen(app.get("port"));
+
+console.log("Server on port", app.get('port'));
